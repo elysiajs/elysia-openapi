4af8db43a7025018f2e5b0e6fdeb03bc34cf5eeb
Marco Pasqualetti <marco.pasqualetti@live.com>
2025-10-08 14:08:38 +0200

    🔧 fix: avoid error when using `exclude.tags` with an API without tags

--- src/openapi.ts
+++ src/openapi.ts
@@ -320,7 +320,7 @@ export function toOpenAPISchema(
 			continue
 
 		const hooks: InputSchema & {
-			detail: Partial<OpenAPIV3.OperationObject>
+			detail?: Partial<OpenAPIV3.OperationObject>
 		} = route.hooks ?? {}
 
 		if (references?.length)
@@ -380,7 +380,7 @@ export function toOpenAPISchema(
 
 		if (
 			excludeTags &&
-			hooks.detail.tags?.some((tag) => excludeTags?.includes(tag))
+			hooks.detail?.tags?.some((tag) => excludeTags?.includes(tag))
 		)
 			continue
 
