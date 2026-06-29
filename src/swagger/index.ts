ffdb3feb43aa900fe900cb46b1d5379a96f7b524
saltyaom <saltyaom@gmail.com>
2026-06-29 23:38:19 +0700

    :broom: chore: cleanup stuff

--- src/swagger/index.ts
+++ src/swagger/index.ts
@@ -109,6 +109,7 @@ export const SwaggerUIRender = (
         name="og:description"
         content="${info.description}"
     />
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦊</text></svg>" />
     ${
 		autoDarkMode && typeof theme === 'string'
 			? `<style>
